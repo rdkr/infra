2893fa15f4fc7c4120e39645d505937a776ca6a5
Neel Radhakrishnan <neel@rdkr.uk>
2020-04-11 16:15:39 +0100

    tokens

--- csgo.tf
+++ csgo.tf
@@ -1,4 +1,8 @@
-variable "csgo_gslt_token" {
+variable "csgo_gslt_token_dm" {
+  type = string
+}
+
+variable "csgo_gslt_token_pug" {
   type = string
 }
 
@@ -28,10 +32,9 @@ resource "digitalocean_droplet" "csgo" {
   size     = "s-1vcpu-2gb"
   ssh_keys = [26584190]
   user_data = templatefile("${path.module}/csgo/dm/cloud-config.yaml", {
-    csgo_gslt_token    = var.csgo_gslt_token
+    csgo_gslt_token    = var.csgo_gslt_token_dm
     csgo_web_token     = var.csgo_web_token
     csgo_rcon_password = var.csgo_rcon_password
-    csgo_sv_password   = var.csgo_sv_password
   })
 }
 
@@ -61,9 +64,8 @@ resource "digitalocean_droplet" "pug" {
   region   = "lon1"
   size     = "s-1vcpu-2gb"
   ssh_keys = [26584190]
-  user_data = templatefile("${path.module}/csgo.yaml", {
-    csgo_gslt_token    = var.csgo_gslt_token
-    csgo_web_token     = var.csgo_web_token
+  user_data = templatefile("${path.module}/pug/cloud-config.yaml", {
+    csgo_gslt_token    = var.csgo_gslt_token_pug
     csgo_rcon_password = var.csgo_rcon_password
     csgo_sv_password   = var.csgo_sv_password
   })
