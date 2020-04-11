99219b10901bcc029573c23104b645c2ce52e725
Neel Radhakrishnan <neel@rdkr.uk>
2020-04-11 16:12:28 +0100

    2server

--- csgo.tf
+++ csgo.tf
@@ -27,7 +27,7 @@ resource "digitalocean_droplet" "csgo" {
   region   = "lon1"
   size     = "s-1vcpu-2gb"
   ssh_keys = [26584190]
-  user_data = templatefile("${path.module}/csgo.yaml", {
+  user_data = templatefile("${path.module}/csgo/dm/cloud-config.yaml", {
     csgo_gslt_token    = var.csgo_gslt_token
     csgo_web_token     = var.csgo_web_token
     csgo_rcon_password = var.csgo_rcon_password
@@ -42,7 +42,41 @@ resource "digitalocean_volume_attachment" "foobar" {
 
 resource "cloudflare_record" "cs_rdkr_uk" {
   zone_id = "f6d706e06f5fdb858d0c78bafb1194ec"
-  name    = "cs"
+  name    = "dm"
+  value   = digitalocean_droplet.csgo.ipv4_address
+  type    = "A"
+  ttl     = 1
+}
+
+resource "digitalocean_volume" "pug" {
+  region                  = "lon1"
+  name                    = "pug.rdkr.uk"
+  size                    = 50
+  initial_filesystem_type = "ext4"
+}
+
+resource "digitalocean_droplet" "pug" {
+  name     = "pug.rdkr.uk"
+  image    = "ubuntu-18-04-x64"
+  region   = "lon1"
+  size     = "s-1vcpu-2gb"
+  ssh_keys = [26584190]
+  user_data = templatefile("${path.module}/csgo.yaml", {
+    csgo_gslt_token    = var.csgo_gslt_token
+    csgo_web_token     = var.csgo_web_token
+    csgo_rcon_password = var.csgo_rcon_password
+    csgo_sv_password   = var.csgo_sv_password
+  })
+}
+
+resource "digitalocean_volume_attachment" "pug" {
+  droplet_id = digitalocean_droplet.csgo.id
+  volume_id  = digitalocean_volume.csgo.id
+}
+
+resource "cloudflare_record" "pug_rdkr_uk" {
+  zone_id = "f6d706e06f5fdb858d0c78bafb1194ec"
+  name    = "pug"
   value   = digitalocean_droplet.csgo.ipv4_address
   type    = "A"
   ttl     = 1
