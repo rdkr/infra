7376211a79f93a2a5312bd4fa93fe0e4a65f96d0
Neel Radhakrishnan <neel@rdkr.uk>
2020-04-13 21:29:12 +0100

    dns

--- csgo.tf
+++ csgo.tf
@@ -76,10 +76,50 @@ resource "digitalocean_volume_attachment" "pug" {
   volume_id  = digitalocean_volume.pug.id
 }
 
-resource "cloudflare_record" "pug_rdkr_uk" {
+resource "cloudflare_record" "dm_rdkr_uk_1" {
+  zone_id = "f6d706e06f5fdb858d0c78bafb1194ec"
+  name    = "dm"
+  value   = "ns1.digitalocean.com"
+  type    = "NS"
+  ttl     = 1
+}
+
+resource "cloudflare_record" "dm_rdkr_uk_2" {
+  zone_id = "f6d706e06f5fdb858d0c78bafb1194ec"
+  name    = "dm"
+  value   = "ns2.digitalocean.com"
+  type    = "NS"
+  ttl     = 1
+}
+
+resource "cloudflare_record" "dm_rdkr_uk_3" {
+  zone_id = "f6d706e06f5fdb858d0c78bafb1194ec"
+  name    = "dm"
+  value   = "ns3.digitalocean.com"
+  type    = "NS"
+  ttl     = 1
+}
+
+resource "cloudflare_record" "pug_rdkr_uk_1" {
   zone_id = "f6d706e06f5fdb858d0c78bafb1194ec"
   name    = "pug"
-  value   = digitalocean_droplet.pug.ipv4_address
-  type    = "A"
+  value   = "ns1.digitalocean.com"
+  type    = "NS"
+  ttl     = 1
+}
+
+resource "cloudflare_record" "pug_rdkr_uk_2" {
+  zone_id = "f6d706e06f5fdb858d0c78bafb1194ec"
+  name    = "pug"
+  value   = "ns2.digitalocean.com"
+  type    = "NS"
+  ttl     = 1
+}
+
+resource "cloudflare_record" "pug_rdkr_uk_3" {
+  zone_id = "f6d706e06f5fdb858d0c78bafb1194ec"
+  name    = "pug"
+  value   = "ns3.digitalocean.com"
+  type    = "NS"
   ttl     = 1
 }
