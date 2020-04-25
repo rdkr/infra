71869b392372a1823c0aff09f04511da8c315c07
Neel Radhakrishnan <neel@rdkr.uk>
2020-04-25 20:46:04 +0100

    wip

--- core/main.tf
+++ core/main.tf
@@ -8,16 +8,8 @@ terraform {
   }
 }
 
-variable "cloudflare_token" {
-  type = string
-}
-
 provider "cloudflare" {
   api_token = var.cloudflare_token
-}
-
-variable "do_token" {
-  type = string
 }
 
 provider "digitalocean" {
---
+++ core/variables.tf
@@ -0,0 +1,27 @@
+variable "cloudflare_token" {
+  type = string
+}
+
+variable "do_token" {
+  type = string
+}
+
+variable "CSGO_GSLT_TOKEN_DM" {
+  type = string
+}
+
+variable "CSGO_GSLT_TOKEN_PUG" {
+  type = string
+}
+
+variable "CSGO_WEB_TOKEN_DM" {
+  type = string
+}
+
+variable "CSGO_RCON_PASSWORD" {
+  type = string
+}
+
+variable "CSGO_SV_PASSWORD" {
+  type = string
+}
