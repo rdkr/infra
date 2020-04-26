6df27bfa20c73c2f0e9e89238cb10ed2c4c82038
Neel Radhakrishnan <neel@rdkr.uk>
2020-04-26 10:51:17 +0100

    update k8s;gp

--- core/dns_cs.tf
+++ core/dns_cs.tf
@@ -45,3 +45,27 @@ resource "cloudflare_record" "pug_rdkr_uk_3" {
   type    = "NS"
   ttl     = 1
 }
+
+resource "cloudflare_record" "k8s_rdkr_uk_1" {
+  zone_id = "f6d706e06f5fdb858d0c78bafb1194ec"
+  name    = "k8s"
+  value   = "ns1.digitalocean.com"
+  type    = "NS"
+  ttl     = 1
+}
+
+resource "cloudflare_record" "k8s_rdkr_uk_2" {
+  zone_id = "f6d706e06f5fdb858d0c78bafb1194ec"
+  name    = "k8s"
+  value   = "ns2.digitalocean.com"
+  type    = "NS"
+  ttl     = 1
+}
+
+resource "cloudflare_record" "k8s_rdkr_uk_3" {
+  zone_id = "f6d706e06f5fdb858d0c78bafb1194ec"
+  name    = "k8s"
+  value   = "ns3.digitalocean.com"
+  type    = "NS"
+  ttl     = 1
+}
