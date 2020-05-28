ed77d4122dc1e698164602b226c4905ae53e06d8
Neel Radhakrishnan <neel@rdkr.uk>
2020-05-28 20:56:29 +0100

    fix rds

--- core/aws.tf
+++ core/aws.tf
@@ -6,26 +6,77 @@ module "vpc" {
 
   azs             = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
   public_subnets  = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+
+  enable_dns_hostnames = true
+  enable_dns_support   = true
 }
+
+resource "aws_security_group" "db" {
+  name        = "db"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress {
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+
 module "db" {
-  source  = "terraform-aws-modules/rds-aurora/aws"
+  source  = "terraform-aws-modules/rds/aws"
 
-  name                            = "db"
+  identifier = "db"
 
-  engine                          = "aurora-postgresql"
-  engine_version                  = "11.6"
+  engine            = "postgres"
+  engine_version    = "12.2"
+  instance_class    = "db.t2.micro"
+  allocated_storage = 5
+  storage_encrypted = false
 
-  vpc_id                          = module.vpc.vpc_id
-  subnets                         = module.vpc.public_subnets
+  username = "root"
+  password = var.HERMES_DB_PW
+  port     = "5432"
 
-  replica_count                   = 1
-//  allowed_security_groups         = ["sg-12345678"]
-  allowed_cidr_blocks             = ["0.0.0.0/0"]
-  instance_type                   = "db.t2.micro"
-  storage_encrypted               = true
-  apply_immediately               = true
-//  monitoring_interval             = 10
+  vpc_security_group_ids = [aws_security_group.db.id]
 
-//  db_parameter_group_name         = "default"
-//  db_cluster_parameter_group_name = "default"
+  maintenance_window = "Mon:00:00-Mon:03:00"
+  backup_window      = "03:00-06:00"
+
+  # disable backups to create DB faster
+  backup_retention_period = 0
+
+  enabled_cloudwatch_logs_exports = ["postgresql", "upgrade"]
+
+  # DB subnet group
+  subnet_ids = module.vpc.public_subnets
+
+  # DB parameter group
+  family = "postgres12"
+
+  # DB option group
+  major_engine_version = "12"
+
+  # Snapshot name upon DB deletion
+  final_snapshot_identifier = "db"
+
+  # Database Deletion Protection
+  deletion_protection = false
+
+  publicly_accessible = true
+}
+
+provider "postgresql" {
+  host            = module.db.this_db_instance_address
+  port            = 5432
+  database        = "postgres"
+  username        = "root"
+  password        = var.HERMES_DB_PW
+  sslmode         = "require"
+  connect_timeout = 15
+}
+
+resource "postgresql_database" "hermes" {
+  name              = "hermes"
 }
