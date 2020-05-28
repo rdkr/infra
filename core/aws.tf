e03079c4ab8501d4fc6c40bd9dcbf600a9acce3b
Neel Radhakrishnan <neel@rdkr.uk>
2020-05-28 20:05:17 +0100

    add aws

--- core/aws.tf
+++ core/aws.tf
@@ -24,8 +24,8 @@ module "db" {
   instance_type                   = "db.t2.micro"
   storage_encrypted               = true
   apply_immediately               = true
-  monitoring_interval             = 10
+//  monitoring_interval             = 10
 
-  db_parameter_group_name         = "default"
-  db_cluster_parameter_group_name = "default"
+//  db_parameter_group_name         = "default"
+//  db_cluster_parameter_group_name = "default"
 }
