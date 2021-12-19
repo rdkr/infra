f99a1601737cd16a2321427210def42e73283825
Neel Radhakrishnan <neel@rdkr.uk>
2021-12-19 19:15:41 +0000

    backups and fixes

--- gcp/minecraft/minecraft.tf
+++ gcp/minecraft/minecraft.tf
@@ -3,6 +3,20 @@ resource "google_service_account" "minecraft" {
   display_name = "minecraft"
 }
 
+resource "google_storage_bucket" "minecraft" {
+  name          = "mineecraft-rdkr-uk"
+  location      = "EU"
+  versioning {
+    enabled = true
+  }
+}
+
+resource "google_storage_bucket_iam_member" "member" {
+  bucket = google_storage_bucket.minecraft.name
+  role = "roles/storage.objectAdmin"
+  member = "serviceAccount:${google_service_account.minecraft.email}"
+}
+
 resource "google_compute_firewall" "minecraft" {
   name    = "minecraft"
   network = "default"
@@ -14,7 +28,7 @@ resource "google_compute_firewall" "minecraft" {
 
   allow {
     protocol = "udp"
-    ports    = ["25565", "19132", "19133"]
+    ports    = ["25565"]
   }
 
   source_ranges = ["0.0.0.0/0"]
@@ -22,12 +36,12 @@ resource "google_compute_firewall" "minecraft" {
 }
 
 resource "google_compute_instance" "minecraft" {
+  allow_stopping_for_update = true
 
   desired_status = var.on ? "RUNNING" : "TERMINATED"
 
   name         = "minecraft"
-  machine_type = "e2-small"
-  zone         = "europe-west4-a"
+  machine_type = "custom-2-4096"
 
   tags = [google_compute_firewall.minecraft.name]
 
@@ -54,8 +68,8 @@ resource "google_compute_instance" "minecraft" {
 
 resource "cloudflare_record" "minecraft" {
   count   = var.on ? 1 : 0
-  zone_id = "f6d706e06f5fdb858d0c78bafb1194ec"
-  name    = "mc.rdkr.uk"
+  zone_id = "0e5558d4ed3e02955e69c004f375f82f"
+  name    = "mc.tbcuk.xyz"
   value   = google_compute_instance.minecraft.network_interface.0.access_config.0.nat_ip
   type    = "A"
   ttl     = 60
